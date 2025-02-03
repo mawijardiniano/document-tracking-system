4287e6f649a2aa75a3910b7e9a8bf9b1b1730d74
Mawi <jardinianomarklouisse@gmail.com>
2025-02-03 09:21:26 +0800

    create a app.js file

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,23 @@
-dc
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Database connected successfully'))
+  .catch(err => console.log('Database connection error: ', err));
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
